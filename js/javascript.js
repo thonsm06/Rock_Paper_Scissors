36bba7a0d07d9f8f2ab6dd994f919e49fbaf0b83
P51 <116685524+thonsm06@users.noreply.github.com>
2022-10-29 21:12:13 -0500

    Completed many functions
    getComputerChoice, playRound, playerSelection.
    Started work on game() function.

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,70 @@
-console.log("aasdf");
+function getComputerChoice() {
+    let choice = ['rock', 'paper', 'scissors'];
+    let random = choice[Math.floor(Math.random() * choice.length)];
+    return random;
+}
+
+function playRound(playerSelection, getComputerSelection) {
+    if (playerSelection == 'rock')
+    {
+        if (getComputerSelection == 'rock')
+        {
+            return "You tie!";
+        }
+        else if (getComputerSelection == 'paper')
+        {
+            return "You lose!";
+        }
+        else
+        {
+            return "You win!";
+        }
+    }
+    else if (playerSelection == 'paper')
+    {
+        if (getComputerSelection == 'rock')
+        {
+            return "You win!";
+        }
+        else if (getComputerSelection == 'paper')
+        {
+            return "You tie!";
+        }
+        else
+        {
+            return "You lose!";
+        }
+    }
+    else //player is scissors
+    {
+        if (getComputerSelection == 'rock')
+        {
+            return "You lose!";
+        }
+        else if (getComputerSelection == 'paper')
+        {
+            return "You win!";
+        }
+        else
+        {
+            return "You tie!";
+        }
+    }
+
+}
+const getComputerSelection = getComputerChoice();
+let string = prompt("Enter Rock, Paper, Scissors");
+playerSelection = string.toLowerCase();
+if (playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissors')
+{
+    console.log(playerSelection, getComputerSelection);
+}
+
+function game(){
+    for (let i = 0; i < 5; i++)
+    {
+        playRound(playerSelection, getComputerSelection);
+    }
+}
+
+
